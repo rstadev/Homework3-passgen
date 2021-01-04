42e22b5d8befe9a1a549c97ff8b6de44416aefef
unknown <nukasine@gmail.com>
2021-01-04 13:53:48 -0500

    finished off defining the variables in the generatePassword function to meet criteria in class readme. May change the upperOrLower around from here, not sure if a prompt is the most elegant solution. Also added consolelog for all variables I assigned.

--- script.js
+++ script.js
@@ -3,12 +3,15 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   var characters = prompt("How many characters would you like in your password?")
-  var specialChar = confirm("Would you like any special characters like !, #, %, *, etc?")
-
+  var specialChar = confirm("Would you like to include special characters like !, #, %, *, etc?")
+  var numbers = confirm("Would you like to include numbers in your password?")
+  var upperOrLower = prompt("Would you like to include uppercase letters? Choose u, for only upper, l for only lower, or b for both.")
 
 
   console.log("characters: ", characters)
   console.log("special characters: ", specialChar)
+  console.log("numbers chosen: ", numbers)
+  console.log("upperOrLower response: ", upperOrLower)
 }
 
 // Write password to the #password input

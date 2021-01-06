33456c47610a21658ffbd055f530af887b9d4cfa
unknown <nukasine@gmail.com>
2021-01-06 14:56:56 -0500

    Created a charset and passwordEmpty variable for the passwordgen forloop. Created forloop and console.log to check its functionality. added password parameter to writePassword(not sure if needed). added return to function to give it a value that is then passed down to writePassword function. This got the minimal charSet defined to generate a password after all prompts are given, and that reflects input from user.

--- script.js
+++ script.js
@@ -7,26 +7,31 @@ function generatePassword() {
   //   alert("Password cannot be less than 8 characters.")
   // }
   while (true) {
-    var characters = prompt("How many characters would you like in your password?")
+    var characters = prompt("How many characters would you like in your password?");
     if (characters >= 8) {
       break
     }
     alert("You've entered an invalid answer. Please try again")
   }
   var specialChar = confirm("Would you like to include special characters like !, #, %, *, etc?")
-  var numbers = confirm("Would you like to include numbers in your password?")
-  var upperOrLower = prompt("Would you like to include uppercase letters? Choose u, for only upper, l for only lower, or b for both.")
-
-  
-
+  var numbers = confirm("Would you like to include numbers in your password?");
+  var upperOrLower = prompt("Would you like to include uppercase letters? Choose u, for only upper, l for only lower, or b for both.");
+  var passwordEmpty = "";
+  var charSet = "abcdefghijklmnopqrstuvwxyz";
+  for (var i = 0; i < characters; i++) {
+    passwordEmpty += charSet.charAt(Math.floor(Math.random() * charSet.length))
+  }
+   
+  console.log("password: ", passwordEmpty)
   console.log("characters: ", characters)
   console.log("special characters: ", specialChar)
   console.log("numbers chosen: ", numbers)
   console.log("upperOrLower response: ", upperOrLower)
+  return passwordEmpty
 }
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(password) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
